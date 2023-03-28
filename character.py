a6a4c22d4a916f885f3a7be17f0d525fdff02a77
Lee Juneyoung <wnsdud318@gmail.com>
2023-03-28 20:24:44 +0900

    balance update

--- character.py
+++ character.py
@@ -25,7 +25,8 @@ class Character:                # 첫 캐릭터
         attack_count = 1
 
         for i in range(attack_count):
-            damage = random.randint(min_stat_attack, max_stat_attack)
+            damage = random.randint(
+                min_stat_attack, max_stat_attack) - other.physical_defense_power
             other.hp -= damage
 
             print(f"{Colors.GREEN}{self.name}{Colors.RESET}의 공격! {Colors.RED}{other.name}{Colors.RESET}에게 {Colors.YELLOW}일반공격{Colors.RESET}({Colors.YELLOW}{damage}{Colors.RESET}){Colors.RESET}을(를) 입혔습니다.")
@@ -76,6 +77,8 @@ class Knight(Character):        # 검사, 추후 레벨10부터 가능하게?
         # self.hp = 50 + str_stat * 5 + dex_stat * 1
         self.skill_name = "파워 스트라이크"
         self.skill_mp = 12
+        self.knight_constant = 0.6
+        self.knight_skill_constant = 2.6
 
     def get_job(self):
         print(f"{Colors.RESET}{Colors.RED}검사{Colors.RESET}로 전직하셨습니다.")
@@ -83,12 +86,13 @@ class Knight(Character):        # 검사, 추후 레벨10부터 가능하게?
 
     def physical_attack(self, other):
         max_stat_attack = round((self.str_stat * 4 + self.dex_stat) *
-                                0.3)     # (주스탯*4+부스탯) * 직업계수
-        min_stat_attack = round(0.6 * max_stat_attack)     # 숙련도% × 최대스공
+                                self.knight_constant)     # (주스탯*4+부스탯) * 직업계수
+        min_stat_attack = round(0.7 * max_stat_attack)     # 숙련도% × 최대스공
         attack_count = 1
 
         for i in range(attack_count):
-            damage = random.randint(min_stat_attack, max_stat_attack)
+            damage = random.randint(
+                min_stat_attack, max_stat_attack) - other.physical_defense_power
             other.hp -= damage
 
             print(f"{Colors.GREEN}{self.name}{Colors.RESET}의 공격! {Colors.RED}{other.name}{Colors.RESET}에게 {Colors.YELLOW}일반공격{Colors.RESET}({Colors.YELLOW}{damage}{Colors.RESET}){Colors.RESET}을(를) 입혔습니다.")
@@ -102,13 +106,14 @@ class Knight(Character):        # 검사, 추후 레벨10부터 가능하게?
             self.mp -= self.skill_mp
 
             max_stat_attack = round((self.str_stat * 4 + self.dex_stat) *
-                                    0.3 * 2.6)     # (주스탯*4+부스탯) * 직업계수 * 스킬계수
-            min_stat_attack = round(0.6 * max_stat_attack)     # 숙련도% × 최대스공
+                                    self.knight_constant * 2.6)     # (주스탯*4+부스탯) * 직업계수 * 스킬계수
+            min_stat_attack = round(0.7 * max_stat_attack)     # 숙련도% × 최대스공
             attack_count = 1
 
             print(f"{Colors.GREEN}{self.name}{Colors.RESET}의 공격! {Colors.BLUE}마나{Colors.RESET}({Colors.BLUE}{self.skill_mp}{Colors.RESET})를 소모하여")
             for i in range(attack_count):
-                damage = random.randint(min_stat_attack, max_stat_attack)
+                damage = random.randint(
+                    min_stat_attack, max_stat_attack) - other.physical_defense_power
                 other.hp -= damage
 
                 print(f"{Colors.RED}{other.name}{Colors.RESET}에게 {Colors.BLUE}{self.skill_name}{Colors.RESET}({Colors.BLUE}{damage}{Colors.RESET}){Colors.RESET}을(를) 입혔습니다.")
@@ -128,6 +133,7 @@ class Archer(Character):        # 아처
         # self.hp = 50 + str_stat * 5 + dex_stat * 1
         self.skill_name = "애로우 블로우"
         self.skill_mp = 11
+        self.archer_constant = 0.5
 
     def get_job(self):
         print(f"{Colors.RESET}{Colors.RED}아처{Colors.RESET}로 전직하셨습니다.")
@@ -135,13 +141,14 @@ class Archer(Character):        # 아처
 
     def physical_attack(self, other):
         max_stat_attack = round((self.dex_stat * 4 + self.str_stat) *
-                                0.15)     # (주스탯*4+부스탯) * 직업계수
+                                self.archer_constant)     # (주스탯*4+부스탯) * 직업계수
         min_stat_attack = round(0.8 * max_stat_attack)     # 숙련도% × 최대스공
 
         attack_count = 1
 
         for i in range(attack_count):
-            damage = random.randint(min_stat_attack, max_stat_attack)
+            damage = random.randint(
+                min_stat_attack, max_stat_attack) - other.physical_defense_power
             other.hp -= damage
 
             print(f"{Colors.GREEN}{self.name}{Colors.RESET}의 공격! {Colors.RED}{other.name}{Colors.RESET}에게 {Colors.YELLOW}일반공격{Colors.RESET}({Colors.YELLOW}{damage}{Colors.RESET}){Colors.RESET}을(를) 입혔습니다.")
@@ -155,13 +162,14 @@ class Archer(Character):        # 아처
             self.mp -= self.skill_mp
 
             max_stat_attack = round((self.str_stat * 4 + self.dex_stat) *
-                                    0.3 * 1.6)     # (주스탯*4+부스탯) * 직업계수 * 스킬계수
+                                    self.archer_constant * 1.6)     # (주스탯*4+부스탯) * 직업계수 * 스킬계수
             min_stat_attack = round(0.6 * max_stat_attack)     # 숙련도% × 최대스공
             attack_count = 3
 
             print(f"{Colors.GREEN}{self.name}{Colors.RESET}의 공격! {Colors.BLUE}마나{Colors.RESET}({Colors.BLUE}{self.skill_mp}{Colors.RESET})를 소모하여")
             for i in range(attack_count):
-                damage = random.randint(min_stat_attack, max_stat_attack)
+                damage = random.randint(
+                    min_stat_attack, max_stat_attack) - other.physical_defense_power
                 other.hp -= damage
 
                 print(f"{Colors.RED}{other.name}{Colors.RESET}에게 {Colors.BLUE}{self.skill_name}{Colors.RESET}({Colors.BLUE}{damage}{Colors.RESET}){Colors.RESET}을(를) 입혔습니다.")
@@ -181,6 +189,7 @@ class Magician(Character):      # 매지션
         # self.hp = 50 + str_stat * 2 + dex_stat * 1
         self.skill_name = "매직 클로"
         self.skill_mp = 20
+        self.magician_constant = 0.3
 
     def get_job(self):
         print(f"{Colors.RESET}{Colors.RED}매지션{Colors.RESET}로 전직하셨습니다.")
@@ -188,12 +197,13 @@ class Magician(Character):      # 매지션
 
     def physical_attack(self, other):       # 매지션은 일반공격이 매우 약하지
         max_stat_attack = round((self.str_stat * 4 + self.dex_stat) *
-                                0.1)     # (주스탯*4+부스탯) * 직업계수
+                                self.magician_constant)     # (주스탯*4+부스탯) * 직업계수
         min_stat_attack = round(0.4 * max_stat_attack)     # 숙련도% × 최대스공
         attack_count = 1
 
         for i in range(attack_count):
-            damage = random.randint(min_stat_attack, max_stat_attack)
+            damage = random.randint(
+                min_stat_attack, max_stat_attack) - other.physical_defense_power
             other.hp -= damage
 
             print(f"{Colors.GREEN}{self.name}{Colors.RESET}의 공격! {Colors.RED}{other.name}{Colors.RESET}에게 {Colors.YELLOW}일반공격{Colors.RESET}({Colors.YELLOW}{damage}{Colors.RESET}){Colors.RESET}을(를) 입혔습니다.")
@@ -207,13 +217,14 @@ class Magician(Character):      # 매지션
             self.mp -= self.skill_mp
 
             max_stat_attack = round((self.int_stat * 4 + self.luk_stat) *
-                                    0.4 * 2.5)     # (주스탯*4+부스탯) * 직업계수 * 스킬계수
+                                    self.magician_constant * 4)     # (주스탯*4+부스탯) * 직업계수 * 스킬계수
             min_stat_attack = round(0.6 * max_stat_attack)     # 숙련도% × 최대스공
             attack_count = 2
 
             print(f"{Colors.GREEN}{self.name}{Colors.RESET}의 공격! {Colors.BLUE}마나{Colors.RESET}({Colors.BLUE}{self.skill_mp}{Colors.RESET})를 소모하여")
             for i in range(attack_count):
-                damage = random.randint(min_stat_attack, max_stat_attack)
+                damage = random.randint(
+                    min_stat_attack, max_stat_attack) - other.physical_defense_power
                 other.hp -= damage
 
                 print(f"{Colors.RED}{other.name}{Colors.RESET}에게 {Colors.BLUE}{self.skill_name}{Colors.RESET}({Colors.BLUE}{damage}{Colors.RESET}){Colors.RESET}을(를) 입혔습니다.")
@@ -233,6 +244,7 @@ class Rogue(Character):         # 로그
         # self.hp = 50 + str_stat * 2 + dex_stat * 1
         self.skill_name = "럭키★세븐"
         self.skill_mp = 14
+        self.rogue_constant = 0.7
 
     def get_job(self):
         print(f"{Colors.RESET}{Colors.RED}로그{Colors.RESET}로 전직하셨습니다.")
@@ -240,12 +252,13 @@ class Rogue(Character):         # 로그
 
     def physical_attack(self, other):       # 매지션은 일반공격이 매우 약하지
         max_stat_attack = round((self.luk_stat * 4 + self.dex_stat) *
-                                0.25)     # (주스탯*4+부스탯) * 직업계수
+                                self.rogue_constant)     # (주스탯*4+부스탯) * 직업계수
         min_stat_attack = round(0.4 * max_stat_attack)     # 숙련도% × 최대스공
         attack_count = 1
 
         for i in range(attack_count):
-            damage = random.randint(min_stat_attack, max_stat_attack)
+            damage = random.randint(
+                min_stat_attack, max_stat_attack) - other.physical_defense_power
             other.hp -= damage
 
             print(f"{Colors.GREEN}{self.name}{Colors.RESET}의 공격! {Colors.RED}{other.name}{Colors.RESET}에게 {Colors.YELLOW}일반공격{Colors.RESET}({Colors.YELLOW}{damage}{Colors.RESET}){Colors.RESET}을(를) 입혔습니다.")
@@ -259,13 +272,14 @@ class Rogue(Character):         # 로그
             self.mp -= self.skill_mp
 
             max_stat_attack = round((self.luk_stat * 4 + self.dex_stat) *
-                                    0.4 * 1.9)     # (주스탯*4+부스탯) * 직업계수 * 스킬계수
+                                    self.rogue_constant * 1.9)     # (주스탯*4+부스탯) * 직업계수 * 스킬계수
             min_stat_attack = round(0.6 * max_stat_attack)     # 숙련도% × 최대스공
             attack_count = 2
 
             print(f"{Colors.GREEN}{self.name}{Colors.RESET}의 공격! {Colors.BLUE}마나{Colors.RESET}({Colors.BLUE}{self.skill_mp}{Colors.RESET})를 소모하여")
             for i in range(attack_count):
-                damage = random.randint(min_stat_attack, max_stat_attack)
+                damage = random.randint(
+                    min_stat_attack, max_stat_attack) - other.physical_defense_power
                 other.hp -= damage
 
                 print(f"{Colors.RED}{other.name}{Colors.RESET}에게 {Colors.BLUE}{self.skill_name}{Colors.RESET}({Colors.BLUE}{damage}{Colors.RESET}){Colors.RESET}을(를) 입혔습니다.")
